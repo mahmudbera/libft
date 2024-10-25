671ef3ff4e07fd0023f5ad3c51d2c406a3363a07
Mahmud Bera Karagöz <82315858+mahmudbera@users.noreply.github.com>
2024-10-25 19:49:57 +0300

    Delete src directory

--- src/ft_bzero.c
+++
@@ -1,6 +0,0 @@
-#include <string.h>
-
-void bzero(void *b, size_t length)
-{
-    memset(b, '\0', length);
-}
--- src/ft_isprint.c
+++
@@ -1,8 +0,0 @@
-#include "libft.h"
-
-int ft_isprint(int c)
-{
-    if (c >= 0 && c < 32)
-        return (1);
-    return (0);
-}
